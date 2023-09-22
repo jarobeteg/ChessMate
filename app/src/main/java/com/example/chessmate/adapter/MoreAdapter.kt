0dcebf8abe5cbc7bd6e7f240e95d999236e00d80
Tomity Jaroslav <jaroslavtm@gmail.com>
2023-09-22 17:40:18 +0200

    UI improvements and settings activity created

---
+++ app/src/main/java/com/example/chessmate/adapter/MoreAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.chessmate.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chessmate.R
+import com.example.chessmate.ui.activity.SettingsActivity
+
+data class Item(val name: String, val action: () -> Unit)
+class MoreAdapter(private val items: List<Item>) : RecyclerView.Adapter<MoreAdapter.ViewHolder>() {
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.item_more_fragment)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_more_fragment_recycler_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.textView.text = item.name
+        holder.itemView.setOnClickListener{
+            item.action.invoke()
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/chessmate/ui/fragment/MoreFragment.kt
+++ app/src/main/java/com/example/chessmate/ui/fragment/MoreFragment.kt
@@ -1,13 +1,19 @@
 package com.example.chessmate.ui.fragment
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chessmate.R
+import com.example.chessmate.adapter.Item
+import com.example.chessmate.adapter.MoreAdapter
 import com.example.chessmate.ui.ViewModel.MoreViewModel
+import com.example.chessmate.ui.activity.SettingsActivity
 
 class MoreFragment : Fragment() {
 
@@ -21,7 +27,20 @@ class MoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        val view = inflater.inflate(R.layout.fragment_more, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.moreFragment_recyclerView)
+        val items = listOf(
+            Item("Settings Menu") { startActivity(SettingsActivity::class.java) }
+        )
+        val adapter = MoreAdapter(items)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+        return view
+    }
+
+    private fun startActivity(activityClass: Class<*>){
+        val context = requireContext()
+        context.startActivity(Intent(context, activityClass))
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,5 +48,4 @@ class MoreFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MoreViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
