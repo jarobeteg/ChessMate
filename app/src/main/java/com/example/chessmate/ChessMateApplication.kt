2367ea40d53a41198c53475adef1cabdc61d6802
Tomity Jaroslav <jaroslavtm@gmail.com>
2023-10-04 12:13:29 +0200

    Database handling improvement

--- app/src/main/java/com/example/chessmate/ChessMateApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.chessmate
-
-import android.app.Application
-import com.example.chessmate.database.ChessMateDatabase
-
-class ChessMateApplication: Application() {
-    val database by lazy { ChessMateDatabase.getDatabase(this) }
-}
--- app/src/main/java/com/example/chessmate/database/UserProfileRepository.kt
+++ app/src/main/java/com/example/chessmate/database/UserProfileRepository.kt
@@ -1,10 +1,19 @@
 package com.example.chessmate.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import com.example.chessmate.database.dao.UserProfileDAO
 import com.example.chessmate.database.entity.UserProfile
 
-class UserProfileRepository(private val userProfileDAO: UserProfileDAO) {
+class UserProfileRepository(private val context: Context) {
+
+    private val database: ChessMateDatabase by lazy {
+        ChessMateDatabase.getDatabase(context)
+    }
+
+    private val userProfileDAO: UserProfileDAO by lazy {
+        database.userProfileDAO()
+    }
 
     fun getAllUsers(): LiveData<List<UserProfile>>{
         return userProfileDAO.getAllUsers()
