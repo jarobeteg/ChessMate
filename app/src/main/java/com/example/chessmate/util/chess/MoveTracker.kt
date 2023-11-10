04e7bab3fcbfbb9c74a57d6096499c53e2a0a37c
Tomity Jaroslav <jaroslavtm@gmail.com>
2023-11-10 11:18:14 +0100

    Added a continue button for the bottom nav chess menu and made a small update for the move tracker

--- app/src/main/java/com/example/chessmate/util/chess/MoveTracker.kt
+++ app/src/main/java/com/example/chessmate/util/chess/MoveTracker.kt
@@ -3,6 +3,8 @@ package com.example.chessmate.util.chess
 data class MoveTracker(
     val sourceSquare: Square,
     val destinationSquare: Square,
+    val pieceMoved: PieceType,
+    val pieceTaken: PieceType?,
     val turnNumber: Int,
     val isWhiteToMove: Boolean
 )
