53f302d95e3c9df6d4de964c5b5a82cf653e6e52
jarobeteg <jaroslavtm@gmail.com>
2024-06-26 23:57:36 +0200

    added sliding move generator which are basically used for bishop, rook and queen move generation

--- app/src/main/java/com/example/chessmate/util/chess/bitboard/BitboardMoveGenerator.kt
+++ app/src/main/java/com/example/chessmate/util/chess/bitboard/BitboardMoveGenerator.kt
@@ -8,8 +8,8 @@ class BitboardMoveGenerator(private val bitboard: Bitboard) {
         val knightOffsets = listOf(15, 6, 10, 17, -17, -10, -6, -15)
         val bishopOffsets = listOf(9, -9, 7, -7)
         val rookOffsets = listOf(1, -1, 8, -8)
-        val queenOffsets = knightOffsets + rookOffsets
-        val kingOffsets = listOf(1, -1, 8, -8, 9, -9, 7, -7)
+        val queenOffsets = listOf(1, -1, 7, -7, 8, -8, 9, -9)
+        val kingOffsets = listOf(1, -1, 7, -7, 8, -8, 9, -9)
 
         fun isLegalPawnMove(position: Long): Boolean {
             return position != 0L && position and (position - 1) == 0L
@@ -23,6 +23,25 @@ class BitboardMoveGenerator(private val bitboard: Bitboard) {
             val fileDiff = abs(fromFile - toFile)
 
             return fileDiff == 1 || fileDiff == 2
+        }
+
+        fun isSameRankOrFile(fromIndex: Int, toIndex: Int, direction: Int): Boolean {
+            val fromFile = fromIndex % 8
+            val toFile = toIndex % 8
+            val fromRank = fromIndex / 8
+            val toRank = toIndex / 8
+
+            return when (direction) {
+                1, -1 -> fromRank == toRank
+                8, -8 -> fromFile == toFile
+                7, -7 -> abs(fromFile - toFile) == abs(fromRank - toRank)
+                9, -9 -> abs(fromFile - toFile) == abs(fromRank - toRank)
+                else -> false
+            }
+        }
+
+        fun isWithinBounds(index: Int): Boolean {
+            return index in 0..63
         }
     }
 
@@ -47,6 +66,12 @@ class BitboardMoveGenerator(private val bitboard: Bitboard) {
         moves.addAll(generatePawnMoves(bitboard.blackPawns, whitePieces, emptySquares, true))
         moves.addAll(generateKnightMoves(bitboard.whiteKnights, blackPieces, emptySquares))
         moves.addAll(generateKnightMoves(bitboard.blackKnights, whitePieces, emptySquares))
+        moves.addAll(generateBishopMoves(bitboard.whiteBishops, allPieces, blackPieces))
+        moves.addAll(generateBishopMoves(bitboard.blackBishops, allPieces, whitePieces))
+        moves.addAll(generateRookMoves(bitboard.whiteRooks, allPieces, blackPieces))
+        moves.addAll(generateRookMoves(bitboard.blackRooks, allPieces, whitePieces))
+        moves.addAll(generateQueenMoves(bitboard.whiteQueens, allPieces, blackPieces))
+        moves.addAll(generateQueenMoves(bitboard.blackQueens, allPieces, whitePieces))
 
         return moves
     }
@@ -113,6 +138,48 @@ class BitboardMoveGenerator(private val bitboard: Bitboard) {
                 }
             }
             knightsCopy = knightsCopy xor position
+        }
+
+        return moves
+    }
+
+    fun generateBishopMoves(bishops: Long, allPieces: Long, opponentPieces: Long): ArrayDeque<Long> = generateSlidingMoves(bishops, allPieces, opponentPieces, bishopOffsets)
+
+    fun generateRookMoves(rooks: Long, allPieces: Long, opponentPieces: Long): ArrayDeque<Long> = generateSlidingMoves(rooks, allPieces, opponentPieces, rookOffsets)
+
+    fun generateQueenMoves(queens: Long, allPieces: Long, opponentPieces: Long): ArrayDeque<Long> = generateSlidingMoves(queens, allPieces, opponentPieces, queenOffsets)
+
+    //sliding moves such as bishop, rook and queens
+    fun generateSlidingMoves(pieces: Long, allPieces: Long, opponentPieces: Long, directions: List<Int>): ArrayDeque<Long> {
+        val moves = ArrayDeque<Long>()
+        var piecesCopy = pieces
+
+        while (piecesCopy != 0L) {
+            val position = piecesCopy.takeLowestOneBit()
+            val positionIndex = piecesCopy.countTrailingZeroBits()
+
+            for (direction in directions) {
+                var targetIndex = positionIndex
+                while (true) {
+                    targetIndex += direction
+
+                    if (!isWithinBounds(targetIndex) || !isSameRankOrFile(positionIndex, targetIndex, direction)) {
+                        break
+                    }
+
+                    val target = 1L shl targetIndex
+
+                    if (target and allPieces != 0L) {
+                        if (target and opponentPieces != 0L) {
+                            moves.add(target)
+                        }
+                        break
+                    }
+                    moves.add(target)
+                }
+            }
+
+            piecesCopy = piecesCopy xor position
         }
 
         return moves
