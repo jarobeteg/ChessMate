1759d0975aa26e5b7b6bdf55a6e53d5685921c4b
jarobeteg <jaroslavtm@gmail.com>
2024-09-24 11:19:35 +0200

    updated the puzzle fragment UI. added locks for the puzzles. the user can only unlock them by increasing their rating through completing lessons

--- app/src/main/java/com/example/chessmate/ui/fragment/PuzzlesFragment.kt
+++ app/src/main/java/com/example/chessmate/ui/fragment/PuzzlesFragment.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import com.example.chessmate.R
+import com.example.chessmate.database.entity.UserProfile
 import com.example.chessmate.ui.activity.AdvancedPuzzlesActivity
 import com.example.chessmate.ui.activity.BeginnerPuzzlesActivity
 import com.example.chessmate.ui.activity.IntermediatePuzzlesActivity
@@ -16,6 +18,11 @@ import com.example.chessmate.ui.viewmodel.PuzzlesViewModel
 import com.example.chessmate.util.UserProfileManager
 
 class PuzzlesFragment : Fragment() {
+    private lateinit var beginnerPuzzlesButton: Button
+    private lateinit var intermediatePuzzlesButton: Button
+    private lateinit var advancedPuzzlesButton: Button
+    private var userProfile: UserProfile? = null
+    private val userProfileManager = UserProfileManager.getInstance()
 
     companion object {
         fun newInstance() = PuzzlesFragment()
@@ -28,16 +35,34 @@ class PuzzlesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_puzzles, container, false)
+        userProfile = userProfileManager.getUserProfileLiveData().value
 
-        val beginnerPuzzlesButton = view.findViewById<Button>(R.id.beginner_puzzles)
-        val intermediatePuzzlesButton = view.findViewById<Button>(R.id.intermediate_puzzles)
-        val advancedPuzzlesButton = view.findViewById<Button>(R.id.advanced_puzzles)
+        beginnerPuzzlesButton = view.findViewById(R.id.beginner_puzzles)
+        intermediatePuzzlesButton = view.findViewById(R.id.intermediate_puzzles)
+        advancedPuzzlesButton = view.findViewById(R.id.advanced_puzzles)
 
-        beginnerPuzzlesButton.setOnClickListener { animateButton(beginnerPuzzlesButton, 0) }
-        intermediatePuzzlesButton.setOnClickListener { animateButton(intermediatePuzzlesButton, 1) }
-        advancedPuzzlesButton.setOnClickListener { animateButton(advancedPuzzlesButton, 2) }
+        beginnerPuzzlesButton.setOnClickListener { checkPuzzleButtonLock(beginnerPuzzlesButton, 1) }
+        intermediatePuzzlesButton.setOnClickListener { checkPuzzleButtonLock(intermediatePuzzlesButton, 2) }
+        advancedPuzzlesButton.setOnClickListener { checkPuzzleButtonLock(advancedPuzzlesButton, 3) }
+
+        updateLocks()
 
         return view
+    }
+
+    private fun checkPuzzleButtonLock(button: Button, id: Int) {
+        if (userProfile == null) {
+            loginToSolvePuzzles()
+            return
+        }
+
+        if (userProfile!!.level == 0) {
+            loginToSolvePuzzles()
+        } else if (id <= userProfile!!.level) {
+            animateButton(button, id)
+        } else {
+            showLockMessage()
+        }
     }
 
     private fun animateButton(button: Button, id: Int) {
@@ -55,9 +80,9 @@ class PuzzlesFragment : Fragment() {
             .start()
 
         when (id) {
-            0 -> openBeginnerPuzzles()
-            1 -> openIntermediatePuzzles()
-            2 -> openAdvancedPuzzles()
+            1 -> openBeginnerPuzzles()
+            2 -> openIntermediatePuzzles()
+            3 -> openAdvancedPuzzles()
         }
     }
 
@@ -72,6 +97,39 @@ class PuzzlesFragment : Fragment() {
     private fun openAdvancedPuzzles(){
         val openAdvancedPuzzlesIntent = Intent(requireContext(), AdvancedPuzzlesActivity::class.java)
         startActivity(openAdvancedPuzzlesIntent)
+    }
+
+    private fun updateLocks() {
+        when (userProfile?.level) {
+            1 -> updatePuzzleButtonLock(beginnerPuzzlesButton)
+            2 -> {
+                updatePuzzleButtonLock(beginnerPuzzlesButton)
+                updatePuzzleButtonLock(intermediatePuzzlesButton)
+            }
+            3 -> {
+                updatePuzzleButtonLock(beginnerPuzzlesButton)
+                updatePuzzleButtonLock(intermediatePuzzlesButton)
+                updatePuzzleButtonLock(advancedPuzzlesButton)
+
+            }
+        }
+    }
+
+    private fun updatePuzzleButtonLock(button: Button) {
+        button.setCompoundDrawablesWithIntrinsicBounds(
+            null,
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun loginToSolvePuzzles() {
+        Toast.makeText(requireContext(), getString(R.string.login_to_access_solve_puzzles_message), Toast.LENGTH_LONG).show()
+    }
+
+    private fun showLockMessage() {
+        Toast.makeText(requireContext(), getString(R.string.puzzle_lock_message), Toast.LENGTH_LONG).show()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
