45fbfe0b326f8c04191986c1039ae338d73df395
jarobeteg <jaroslavtm@gmail.com>
2024-09-30 15:20:23 +0200

    Basic tactics added

---
+++ app/src/main/java/com/example/chessmate/ui/activity/lessons/SubLessonLoaderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chessmate.ui.activity.lessons
+
+import android.os.Bundle
+import com.example.chessmate.R
+import com.example.chessmate.ui.activity.AbsThemeActivity
+
+class SubLessonLoaderActivity : AbsThemeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_lesson_loader)
+    }
+}
