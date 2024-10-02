d1dde46eeed69a01617ef68a487431f4c9a2700f
jarobeteg <jaroslavtm@gmail.com>
2024-10-02 15:46:06 +0200

    added the necessary components for the opening lessons

---
+++ app/src/main/java/com/example/chessmate/ui/activity/lessons/OpeningLoaderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chessmate.ui.activity.lessons
+
+import android.os.Bundle
+import com.example.chessmate.R
+import com.example.chessmate.ui.activity.AbsThemeActivity
+
+class OpeningLoaderActivity : AbsThemeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opening_loader)
+    }
+}
