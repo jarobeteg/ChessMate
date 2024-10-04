de6740ad02d9f60ead064dfdbd7376cc82cbc4c3
jarobeteg <jaroslavtm@gmail.com>
2024-10-04 21:25:39 +0200

    added external link for more learning purposes for the user

--- app/src/main/java/com/example/chessmate/adapter/MoreAdapter.kt
+++ app/src/main/java/com/example/chessmate/adapter/MoreAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.chessmate.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chessmate.R
 
-data class Item(val name: String, val action: () -> Unit)
-class MoreAdapter(private val items: List<Item>) : RecyclerView.Adapter<MoreAdapter.ViewHolder>() {
+data class Item(val name: String, val isUrl: Boolean = false, val action: () -> Unit)
+class MoreAdapter(private val items: List<Item>, private val context: Context) : RecyclerView.Adapter<MoreAdapter.ViewHolder>() {
 
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
@@ -27,6 +29,16 @@ class MoreAdapter(private val items: List<Item>) : RecyclerView.Adapter<MoreAdap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.textView.text = item.name
+        if (item.isUrl) {
+            val externalDrawable = ContextCompat.getDrawable(context, R.drawable.ic_external_24)
+            holder.textView.setCompoundDrawablesWithIntrinsicBounds(
+                null,
+                null,
+                externalDrawable,
+                null
+            )
+        }
+
         holder.itemView.setOnClickListener{
             item.action.invoke()
         }
--- app/src/main/java/com/example/chessmate/ui/fragment/MoreFragment.kt
+++ app/src/main/java/com/example/chessmate/ui/fragment/MoreFragment.kt
@@ -1,6 +1,7 @@
 package com.example.chessmate.ui.fragment
 
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -37,9 +38,13 @@ class MoreFragment : Fragment() {
         val recyclerView: RecyclerView = view.findViewById(R.id.moreFragment_recyclerView)
         val items = listOf(
             Item(getString(R.string.title_settings_menu)) { startActivity(SettingsActivity::class.java) },
-            Item(getString(R.string.board_editor_title)) { startActivity(BoardEditorActivity::class.java) }
+            Item(getString(R.string.board_editor_title)) { startActivity(BoardEditorActivity::class.java) },
+            Item(getString(R.string.chess_com_title), true) { openUrl("https://www.chess.com/")},
+            Item(getString(R.string.lichess_org_title), true) { openUrl("https://lichess.org/") },
+            Item(getString(R.string.agadmator_title), true) { openUrl("https://www.youtube.com/@agadmator") },
+            Item(getString(R.string.gotham_chess_title), true) { openUrl("https://www.youtube.com/@GothamChess") },
         )
-        val adapter = MoreAdapter(items)
+        val adapter = MoreAdapter(items, requireContext())
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
     }
@@ -47,6 +52,11 @@ class MoreFragment : Fragment() {
     private fun startActivity(activityClass: Class<*>){
         val context = requireContext()
         context.startActivity(Intent(context, activityClass))
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
